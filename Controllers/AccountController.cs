a3d6cd99f1bb5c89566999ff4c75bb20e4e9abc9
ArgjendIstogu <argjend.istogu@studentet.uni-pr.edu>
2022-05-01 23:16:51 +0200

    Register Page added

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace VisionStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/UserAccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace VisionStore.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required (ErrorMessage = "Full Name is required")]
+        public string FullName { get; set; }
+
+
+
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType (DataType.Password)]
+        public string Password { get; set; }
+
+
+
+        [Required(ErrorMessage = "Please confirm your password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+
+
+
+        [Required(ErrorMessage = "Birthplace is required")]
+        public string Birthplace { get; set; }
+
+
+
+        [Required(ErrorMessage = "Birthday is required")]
+        [DataType(DataType.Date)]
+        public DateOnly Birthday { get; set; }  
+
+
+
+
+    }
+}
