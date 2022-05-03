2d5a2aab4c1a5e6ba640d2d881617f4630f5c8cb
LabiontGjoka <labinoti.gjoka@gmail.com>
2022-05-03 18:14:37 +0200

    Register and Login with Identity User of Microsoft

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using VisionStore.Areas.Identity.Data;
+using VisionStore.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("VisionStoreContextConnection");;
+
+builder.Services.AddDbContext<VisionStoreContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+
+})
+    .AddEntityFrameworkStores<VisionStoreContext>();;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,6 +35,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
@@ -24,4 +43,11 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapRazorPages();
+});
 app.Run();
