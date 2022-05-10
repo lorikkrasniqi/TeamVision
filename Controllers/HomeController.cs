bec36beb13d74502a1e74b46fb76d6c61a80469d
Fatlum Maliqi <fatlummaliqi49554@gmail.com>
2022-05-10 13:32:36 +0200

    added crud operation to product

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using VisionStore.Data;
 using VisionStore.Models;
 
 namespace VisionStore.Controllers
@@ -7,10 +8,11 @@ namespace VisionStore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private  AppDbContext _DbContext { get; set; }
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _DbContext = context;
         }
 
         public IActionResult Index()
@@ -21,6 +23,40 @@ namespace VisionStore.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult Product()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Product(Products model)
+        {
+            var record = new Products()
+            {
+                Title = model.Title,
+                Description = model.Description,
+                Quantity = model.Quantity,
+                Price = model.Price
+            };
+            _DbContext.Products.Add(record);
+            _DbContext.SaveChanges();
+            return RedirectToAction("Product");
+        }
+
+        public IActionResult ListProducts()
+        {
+            var list = _DbContext.Products.ToList();
+            return View(list);
+        }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _DbContext.Products.Where(c => c.ProductId == id).FirstOrDefault();
+            _DbContext.Products.Remove(product);
+            _DbContext.SaveChanges();
+            return RedirectToAction("ListProducts");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Models/Products.cs
+++ Models/Products.cs
@@ -7,11 +7,20 @@ namespace VisionStore.Models
         [Key]
         public int ProductId { get; set; }
 
+        [Display(Name = "Titulli i produktit")]
+        [Required(ErrorMessage = "Fusha titulli i produktit duhet plotesuar")]
         public string Title { get; set; }
 
+        [Display(Name = "Pershkrimi i produktit")]
+        [Required(ErrorMessage = "Pershkrimi i produktit duhet plotesuar")]
         public string Description { get; set; }
+
+        [Display(Name = "Cmimi")]
+        [Required(ErrorMessage = "Fusha cmimi duhet plotesuar")]
         public double Price { get; set; }
 
+        [Display(Name = "Sasia")]
+        [Required(ErrorMessage = "Fusha sasia duhet plotesuar")]
         public int Quantity { get; set; }
 
         public ICollection<OrderedProducts> OrderedProducts { get; set; }
