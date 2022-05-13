c31822965f0ae415958be5f16682d0cb3c938f98
Fatlum Maliqi <fatlummaliqi49554@gmail.com>
2022-05-13 17:44:15 +0200

    crud operations done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using VisionStore.Data;
 using VisionStore.Models;
 
 namespace VisionStore.Controllers
@@ -8,11 +7,10 @@ namespace VisionStore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private  AppDbContext _DbContext { get; set; }
-        public HomeController(ILogger<HomeController> logger, AppDbContext context)
+
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _DbContext = context;
         }
 
         public IActionResult Index()
@@ -23,33 +21,6 @@ namespace VisionStore.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        public IActionResult CreateProduct()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult CreateProduct(Products obj)
-        {
-            _DbContext.Products.Add(obj);
-            _DbContext.SaveChanges();
-            return RedirectToAction("CreateProduct");
-        }
-
-        public IActionResult ReadProduct()
-        {
-            var list = _DbContext.Products;
-            return View(list);
-        }
-
-        public IActionResult DeleteProduct(int id)
-        {
-            var product = _DbContext.Products.Where(c => c.ProductId == id).FirstOrDefault();
-            _DbContext.Products.Remove(product);
-            _DbContext.SaveChanges();
-            return RedirectToAction("ReadProduct");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VisionStore.Data;
+using VisionStore.Models;
+
+namespace VisionStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ILogger<ProductsController> _logger;
+        private AppDbContext _DbContext { get; set; }
+        public ProductsController(ILogger<ProductsController> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _DbContext = context;
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Products product)
+        {
+            _DbContext.Products.Add(product);
+            _DbContext.SaveChanges();
+            return RedirectToAction("Add");
+        }
+
+        public IActionResult All()
+        {
+            var list = _DbContext.Products;
+            return View(list);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _DbContext.Products.Where(c => c.ProductId == id).FirstOrDefault();
+            _DbContext.Products.Remove(product);
+            _DbContext.SaveChanges();
+            return RedirectToAction("All");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var product = _DbContext.Products.Where(c => c.ProductId == id).FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Products product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            await _DbContext.SaveChangesAsync();
+            return RedirectToAction("All");
+        }
+    }
+}
