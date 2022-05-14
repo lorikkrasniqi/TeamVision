c1b117521d75e6d803296153732630209f0f7f56
Argjend <argjend.istogu@studentet.uni-pr.edu>
2022-05-14 12:45:22 +0200

    identityMigration

---
+++ Areas/Identity/Data/VisionStoreUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace VisionStore.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the VisionStoreUser class
+public class VisionStoreUser : IdentityUser
+{
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string? FirstName { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string? LastName { get; set; }    
+}
+
--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using VisionStore.Data;
+using VisionStore.Areas.Identity.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("VisionStoreContextConnection") ?? throw new InvalidOperationException("Connection string 'VisionStoreContextConnection' not found.");
+
+builder.Services.AddDbContext<VisionStoreContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<VisionStoreUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<VisionStoreContext>();;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -17,11 +31,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
