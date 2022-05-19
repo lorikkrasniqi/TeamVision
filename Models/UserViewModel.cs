15b4156f297204c11086d6c152565eea74238e3c
Argjend <argjend.istogu@studentet.uni-pr.edu>
2022-05-19 16:36:23 +0200

    updated

---
+++ Models/UserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using VisionStore.Areas.Identity.Data;
+namespace VisionStore.Models  
+{
+    public class UserViewModel : ApplicationUser
+    {
+        public string Id { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }    
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        
+    }
+}
